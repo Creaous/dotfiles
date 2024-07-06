8326b7b75a37349adb3a48151ab19d0e3f110b8e
Mitchell <68230846+Creaous@users.noreply.github.com>
2024-07-06 11:30:45 +0930

    run stage-2.sh as user

--- ansible/run-all.sh
+++ ansible/run-all.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
-echo "Ensuring the scripts are executable"
+
+# Ensure scripts are executable
 chmod +x stage-1.sh
 chmod +x stage-2.sh
+
+# Get the user from config.yml
+user=$(yq .user config.yml | sed 's/"//g')
+
+# Check if yq is available
+if ! command -v yq &> /dev/null; then
+    sudo pacman -Sy --noconfirm yq
+fi
+
+# Check if user value is found
+if [ -z "$user" ]; then
+  echo "Error: 'user' key not found in config.yml"
+  exit 1
+fi
+
+# Execute stage 1
 echo "Executing stage 1 of the scripts"
 ./stage-1.sh
-echo "Executing stage 2 of the scripts"
-./stage-2.sh
+
+# Execute stage 2 as the retrieved user
+echo "Executing stage 2 of the scripts as user: $user"
+sudo -E -u $user ./stage-2.sh
+
+# (Hopefully) it completed successfully.
 echo "(Hopefully) it completed successfully."
